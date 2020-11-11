a25c935e34f165c6f282d6988dffdcec8538ae8e
Sree Harsha Nelaturu <nelaturu.harsha@gmail.com>
2020-11-12 01:43:53 +0530

    Update test.py

--- GMM/test.py
+++ GMM/test.py
@@ -2,12 +2,12 @@ from gmm import GaussianMixtureModel
 from sklearn.datasets import make_blobs
 import numpy as np
 
-X, y = make_blobs(n_samples = 100, n_features= 3)
+X, y = make_blobs(n_samples = 100, n_features= 2)
 
-gmm = GaussianMixtureModel(num_clusters = 5, num_iters = 1000, data = X)
+gmm = GaussianMixtureModel(num_clusters = 5, num_iters = 100, data = X)
 
 gmm.fit()
 
-print(gmm.predict_cluster(x = np.array([1, 2, 3])))
+print(gmm.predict_cluster(x = np.array([1, 2])))
 
 gmm.plot_clusters()
