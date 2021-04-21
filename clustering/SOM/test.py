e0666c3c2a4407ce5d03763941cb2f8e4bb42bcc
Pranav B <70643852+Pranav1007@users.noreply.github.com>
2021-04-21 19:22:37 +0530

    Update test.py

--- clustering/SOM/test.py
+++ clustering/SOM/test.py
@@ -7,6 +7,6 @@ import matplotlib.pyplot as plt
 from minisom import MiniSom
 
 if __name__ == '__main__':
-    image, data = read_img('Images/Testing/Image3.jpeg')
+    image, data = read_img('Images/Testing/Image1.jpeg')
     starting_weights, reduced_image, learnt_weights = train(image, data)
     plot(image, reduced_image, starting_weights, learnt_weights)
